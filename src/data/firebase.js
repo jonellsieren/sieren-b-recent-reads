19459bb70ae3a4b96e36aa1a08e87976f2cec15e
jonellsieren <70969803+jonellsieren@users.noreply.github.com>
2020-11-08 13:42:17 -0600

    Added new firebase collecton
    Add books and details

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -1,20 +1,20 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBBaLX5UDI8nEMgQ1QZnqVU_XY9DNm2Prw",
-  authDomain: "sieren-code-sprint-b.firebaseapp.com",
-  databaseURL: "https://sieren-code-sprint-b.firebaseio.com",
-  projectId: "sieren-code-sprint-b",
-  storageBucket: "sieren-code-sprint-b.appspot.com",
-  messagingSenderId: "793092123538",
-  appId: "1:793092123538:web:f4e39ce95ff859a915ef4b",
+var firebaseConfig = {
+  apiKey: "AIzaSyB3Q23b1UNJyxLS6-peJf-6rnICzBa8Dl8",
+  authDomain: "recent-reads-64572.firebaseapp.com",
+  databaseURL: "https://recent-reads-64572.firebaseio.com",
+  projectId: "recent-reads-64572",
+  storageBucket: "recent-reads-64572.appspot.com",
+  messagingSenderId: "425786313130",
+  appId: "1:425786313130:web:76ee8921d5b8598fc4c3d3",
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
-const moviesCollection = db.collection("movies");
+const booksCollection = db.collection("books");
 
 export default db;
-export { moviesCollection };
+export { booksCollection };
