f72856b3abfc4ea92de4e7d7f573e0096dac1cf3
jonellsieren <70969803+jonellsieren@users.noreply.github.com>
2020-12-12 15:07:28 -0600

    Account infor page set log in and log out

--- src/components/account-info.js
+++ src/components/account-info.js
@@ -1,26 +1,70 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ErrorMessage from "./error-message";
+import { provider, auth } from "../data/firebase";
 import "./account-info.css";
 
 function AccountInfo(props) {
+  const { user } = props;
   const [errorMessage, setErrorMessage] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
   const signIn = async () => {
     setIsLoading(true);
     setErrorMessage(null);
-    // TODO: Sign in!
+
+    try {
+      await auth.signInWithPopup(provider);
+    } catch (error) {
+      setErrorMessage("Oh no! Please try that again");
+      console.error(error);
+    }
     setIsLoading(false);
   };
 
   const signOut = async () => {
     setIsLoading(true);
     setErrorMessage(null);
-    // TODO: Sign out!
+    try {
+      await auth.signOut();
+    } catch (error) {
+      setErrorMessage("Oh no! You didn't log out, Please try that again");
+      console.error(error);
+    }
     setIsLoading(false);
   };
 
-  let contents = <p>User account info.</p>;
+  let contents;
+  if (user) {
+    const { displayName } = user;
+    contents = (
+      <>
+        <p>Welcome back, {displayName}! Ready to go? log out below.</p>
+        <button
+          className="login-form__button"
+          onClick={signOut}
+          disabled={isLoading}
+        >
+          {isLoading ? "Logging Out..." : "Log Out"}
+        </button>
+      </>
+    );
+  } else {
+    contents = (
+      <>
+        <p>
+          Log in or create a new account by linking your Google account. Follow
+          the instructions in the pop up window.{" "}
+        </p>
+        <button
+          className="login-form__button"
+          onClick={signIn}
+          disabled={isLoading}
+        >
+          {isLoading ? "Logging In..." : "Log In"}
+        </button>
+      </>
+    );
+  }
 
   return (
     <div className="account-container">
--- src/data/firebase.js
+++ src/data/firebase.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
+import "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -13,8 +14,11 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
+const provider = new firebase.auth.GoogleAuthProvider();
+const auth = firebase.auth();
+
 const db = firebase.firestore();
 const booksCollection = db.collection("books");
 
 export default db;
-export { booksCollection };
+export { booksCollection, provider, auth, firebase };
